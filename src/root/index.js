15b2933feb22db0ec0ae4b3b648135fc28a9f7c2
Asadbek <a.nosirjonov@uic.group>
2022-09-24 19:03:57 +0500

    feat: main styles

--- src/root/index.js
+++ src/root/index.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 const Root = () => {
   return (
-    <div>Root</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<h1>home</h1>} />
+        <Route path="/properties" element={<h1>properties</h1>} />
+        <Route path="*" element={<h1>404</h1>} />
+        <Route path="/" element={<Navigate to={"/home"} />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default Root
+export default Root;
